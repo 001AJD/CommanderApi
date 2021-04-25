e62246221d35958a31efc0a2eeed938c1ca4ee25
001AJD <ajinkyadhomne.27@gmail.com>
2021-04-25 09:44:16 +0530

    initial commit

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+	[Route("api/command")]
+	[ApiController]
+	public class CommanderController : ControllerBase
+	{
+		private readonly ICommanderRepo _repository;
+		public CommanderController(ICommanderRepo repository)
+		{
+			_repository = repository;
+		}
+		
+		[HttpGet]
+		public ActionResult <IEnumerable<Command>> GetAllCommands()
+		{
+			var commandItems = _repository.GetAppCommand();
+			return Ok(commandItems);
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult <Command> GetCommandById(int id)
+		{
+			var command = _repository.GetCommandById(id);
+
+			return Ok(command);
+
+		}
+	}
+
+}
---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+	public class MockCommanderRepo : ICommanderRepo
+	{
+		public IEnumerable<Command> GetAppCommand()
+		{
+			var commands = new List<Command>
+			{
+				new Command{Id=0, HowTo="Boil an egg",Line="Boil water", Platform="Kettle  Pan"},
+				new Command{Id=1, HowTo="Make Coffee",Line="Boil Water, Pour Coffee", Platform="Kettle  Pan"},
+				new Command{Id=2, HowTo="Cut Bread",Line="Get a knife", Platform="Kettle  Pan"},
+				new Command{Id=3, HowTo="Make Pizza",Line="Buy Cheese", Platform="Kettle  Pan"},
+			};
+			return commands;
+		}
+
+		public Command GetCommandById(int id)
+		{
+			return new Command{Id=0, HowTo="Boil an egg", Platform="Kettle  Pan"};
+		}
+	}
+}
